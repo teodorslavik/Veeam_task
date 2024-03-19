497c20e56dc16c0ac1cba3e854376e4f86d49147
teodorslavik <59368270+teodorslavik@users.noreply.github.com>
2024-03-19 09:09:50 +0100

    finito

--- sync.py
+++ sync.py
@@ -1,10 +1,13 @@
-import glob, os, pathlib, hashlib, shutil
+import glob, os, pathlib, hashlib, shutil, time
 
 source = "source"
 replica = "replica"
 
 listSource = {}
 listReplica = {}
+
+timeNext = 0
+timeIncrease = 10
 
 def deleteElement(path):  
     elementType = "NONE"
@@ -38,7 +41,7 @@ def copyElement(src, dest):
     f.write("COPY (" + elementType + "): " + str(src) + "\n")
     f.close() 
     
-    print("COPYING: " + src + "to" + dest + " (" + elementType + ")")
+    print("COPYING: " + src + " to " + dest + " (" + elementType + ")")
 
     
     if os.path.isfile(src):
@@ -49,15 +52,18 @@ def copyElement(src, dest):
         shutil.copytree(src, dest)
 
 def hash_file(path):
-   h = hashlib.sha1()
+    if os.path.isdir(path):
+        return 0
+       
+    h = hashlib.sha1()
 
-   with open(path,'rb') as file:
-       chunk = 0
-       while chunk != b'':
-           chunk = file.read(1024)
-           h.update(chunk)
+    with open(path,'rb') as file:
+        chunk = 0
+        while chunk != b'':
+            chunk = file.read(1024)
+            h.update(chunk)
 
-   return h.hexdigest()
+    return h.hexdigest()
 
 def sync():
     # print(os.listdir("./source"))
@@ -70,28 +76,34 @@ def sync():
     listSource = list(pathlib.Path(source).rglob("*")) #list(pathlib.Path(source).glob("*"))
     listReplica = list(pathlib.Path(replica).rglob("*"))  #list(pathlib.Path(replica).glob("*"))
 
-    print(listSource)
-
     # delete unique files from replica folder
     for element in listReplica:
         if(os.path.exists(element)):
             element2 = source + str(element)[len(replica):]
 
-            if(os.path.exists(element2) == False):
-                
+            if(os.path.exists(element2) == False or hash_file(element2) != hash_file(element)):
+
                 deleteElement(str(element))
 
     for element in listSource:
-        if(os.path.exists(element)):
+        if os.path.exists(element):
             element2 = replica + str(element)[len(source):]
 
             if(os.path.exists(element2) == False):
                 
                 copyElement(str(element), str(element2))
+             
+            elif hash_file(element) != hash_file(element2):
+                deleteElement(str(element2))
+                copyElement(str(element), str(element2))
+                
         
 # TODO
-# HASH comparing
-# timming
-# config file
+# log time
+# timmingtime.time()
 
-sync()
+while True:
+    if(time.time() >= timeNext):
+        #print("yes")
+        timeNext = time.time() + timeIncrease
+        sync()
